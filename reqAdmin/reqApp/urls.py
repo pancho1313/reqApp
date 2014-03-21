f02b6bbbf6c4624746952ed084a2baa493e5fdb8
job <yensung.hafon@gmail.com>
2014-03-21 01:39:17 -0300

    navegación básica proyecto

--- reqAdmin/reqApp/urls.py
+++ reqAdmin/reqApp/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import patterns, url
 from reqApp import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.test, name='test'),
-    """
-    url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
-    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
-    url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
-    """
+    url(r'^TU/$', views.viewTU, name='TU'),
+    url(r'^RU/$', views.viewRU, name='RU'),
+    url(r'^RS/$', views.viewRS, name='RS'),
+    url(r'^MD/$', views.viewMD, name='MD'),
+    url(r'^CP/$', views.viewCP, name='CP'),
 )
