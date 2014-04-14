9593548a96d47c1d1853c7b461f0288a5f2f09e9
job <yensung.hafon@gmail.com>
2014-04-14 00:28:03 -0300

    matrices simples funcionando

--- reqAdmin/reqApp/static/js/reqApp.js
+++ reqAdmin/reqApp/static/js/reqApp.js
@@ -52,5 +52,5 @@ function cerrarTodos(){
 // mostrar elementos de fila y columna seleccionada en la matriz de trazado
 function mostrarElementosDeMatrizDeTrazado(elFila, elFilaEstado, elCol, elColEstado){
     document.getElementById('fila').innerHTML = "<b class="+elFilaEstado+">"+elFila+"</b>";
-    document.getElementById('columna').innerHTML = "<b class="+elColEstado+">"+elCol+"</b>";
+    document.getElementById('columna').innerHTML = "<b href='#' class="+elColEstado+">"+elCol+"</b>";
 }
