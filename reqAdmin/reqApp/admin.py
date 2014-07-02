62e25e01839b13c932edc40476c12c52c5fed911
job <yensung.hafon@gmail.com>
2014-07-02 15:38:04 -0400

    admin changes

--- reqAdmin/reqApp/admin.py
+++ reqAdmin/reqApp/admin.py
@@ -4,18 +4,19 @@ from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 
 class UserProfileInline(admin.StackedInline):
-   model = UserProfile
-   max_num = 1
-   can_delete = False
+    model = UserProfile
+    max_num = 1
+    can_delete = False
+    filter_horizontal = ("proyectos",)
 
 class UserAdmin(AuthUserAdmin):
-   def add_view(self, *args, **kwargs):
-      self.inlines = []
-      return super(UserAdmin, self).add_view(*args, **kwargs)
+    def add_view(self, *args, **kwargs):
+        self.inlines = []
+        return super(UserAdmin, self).add_view(*args, **kwargs)
 
-   def change_view(self, *args, **kwargs):
-      self.inlines = [UserProfileInline]
-      return super(UserAdmin, self).change_view(*args, **kwargs)
+    def change_view(self, *args, **kwargs):
+        self.inlines = [UserProfileInline]
+        return super(UserAdmin, self).change_view(*args, **kwargs)
 
 # unregister old user admin
 admin.site.unregister(User)
